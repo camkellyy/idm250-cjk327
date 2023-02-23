6fe757c025e706cba4f3b1241b7bc079e0fb3a2d
cjk327 <cjk327@drexel.edu>
2023-02-23 13:48:20 -0500

    title

--- wp-content/themes/idm250/single-albums.php
+++ wp-content/themes/idm250/single-albums.php
@@ -1,5 +1,5 @@
 <?php get_header();?>
-<h1><?php echo get_the_title(); ?></h1>
+<h1 class="title"><?php echo get_the_title(); ?></h1>
 <?php
 $currentPostId = get_the_id();
 $terms = get_the_terms($currentPostId, 'category');
